4b5fe4045a1694ba0e48e8540f2d48f0fc9de8b0
Pratikp303 <pradhanpratik303@gmail.com>
2026-04-18 10:20:05 +0530

    Update searchRoutes.js

--- searchRoutes.js
+++ searchRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { masterSearch } = require('../controllers/searchController');
+const { masterSearch } = require('./searchController');
 
 // 🔥 MASTER SEARCH
 router.get('/', masterSearch);
